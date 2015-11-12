b0789ea3961fab4bf6936f919c4c896888931f4d
Yifeng Wang <ywang15@babson.edu>
2015-11-12 15:35:46 -0800

    Initialize passport

--- server/server-config.js
+++ server/server-config.js
@@ -3,12 +3,14 @@ var db = require('./db');
 var morgan = require('morgan');
 var parser = require('body-parser');
 var router = require('./routes.js');
+var passport = require('./auth-config');
 
 var app = express();
 
 // Logging and parsing
 app.use(morgan('dev'));
 app.use(parser.json());
+app.use(passport.initialize());
 
 // Set up base router
 app.use("/", router);
