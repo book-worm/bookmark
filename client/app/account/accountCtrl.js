d8e53e290b145c6ba2350516ce8b25b5be70baf2
Colin Seale <ceseale3@gmail.com>
2015-11-12 19:23:48 -0800

    add method calls to service to get user data

--- client/app/account/accountCtrl.js
+++ client/app/account/accountCtrl.js
@@ -1,3 +1,7 @@
 angular.module('account', [])
    .controller('accountCtrl', function ($scope, UserData) {
+    UserData.getUser()
+      .then(function(user){
+        $scope.user = user;
+    });
    });
