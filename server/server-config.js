8fd5063fc79b70b3e7271eab90c5ce330c977803
Colin Seale <ceseale3@gmail.com>
2015-11-15 19:50:59 -0800

    add wildcard to redirect all other routes to index.html

--- server/server-config.js
+++ server/server-config.js
@@ -4,6 +4,7 @@ var morgan = require('morgan');
 var parser = require('body-parser');
 var router = require('./routes.js');
 var passport = require('./auth-config');
+var path = require('path');
 
 var app = express();
 
@@ -17,5 +18,17 @@ app.use("/", router);
 
 // Serve the client files
 app.use(express.static(__dirname + "/../client"));
+
+
+/*
+* This is a fix for the ui router to work in HTML5mode ( without # ) which is needed to allow redirect for oauth
+*/
+
+// All other routes should redirect to the index.html
+app.route('/*')
+  .get(function(req, res) {
+    res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+  });
+
 
 module.exports = app;
