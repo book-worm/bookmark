57d89d4340eedc091436069303b7c4059f406387
Colin Seale <ceseale3@gmail.com>
2015-11-16 06:33:56 -0800

    add routes for Oauth callback

---
+++ server/controllers/authCallbackRoutes.js
@@ -0,0 +1,14 @@
+var passport = require('passport');
+
+module.exports = {
+  get: passport.authenticate('goodreads', { failureRedirect: '/login' , successRedirect: '/browse', session: false }),
+  post: function(req, res) {
+    res.redirect('/');
+  },
+  put: function(req, res) {
+    res.redirect('/');
+  },
+  delete: function(req, res) {
+    res.redirect('/');
+  }
+};
