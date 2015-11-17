69e3ee25e19bb9d9c5e4d4daec91f7d822087dea
Colin Seale <ceseale3@gmail.com>
2015-11-16 18:10:00 -0800

    add serializer

--- server/controllers/successRoutes.js
+++ server/controllers/successRoutes.js
@@ -1,5 +1,7 @@
 module.exports = {
   get: function( req, res){
+    console.log('in succsees!!!!!!!!!!!!!!')
+    console.log(res);
     res.cookie('status', JSON.stringify('true'));
     res.redirect('/browse');
   },
