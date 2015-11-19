e889fcd29660fe82d8cb5859999ac150ef165359
Colin Seale <ceseale3@gmail.com>
2015-11-18 20:18:10 -0800

    explain success controller

--- server/controllers/successRoutes.js
+++ server/controllers/successRoutes.js
@@ -1,5 +1,13 @@
 var currentUser = require('../db/currentUser.js');
 
+/**
+* Endpoint controller for successful Goodreads login
+* it sets the cookies on the header for client with
+* user id and login status and redirects to browse
+* 
+* @type {Object}
+*/
+
 module.exports = {
   get: function( req, res){
     res.cookie('status', JSON.stringify('true'));
