a4d67cc765e560da9b80d2c53f11b321540e3644
Yifeng Wang <yeefom@gmail.com>
2015-11-18 21:25:44 -0800

    Add documentation

--- server/controllers/authRoutes.js
+++ server/controllers/authRoutes.js
@@ -1,5 +1,12 @@
 var passport = require('passport');
 
+/**
+ * Router for '/auth/goodreads', Passport will use 'goodreads' strategy to handle the OAuth
+ * Only GET is needed for OAuth but all the other methods need to exist to work with the server
+ * @type {Object}
+ */
+ 
+
 module.exports = {
   get: passport.authenticate('goodreads'),
   post: function(req, res) {
--- server/controllers/successRoutes.js
+++ server/controllers/successRoutes.js
@@ -1,5 +1,12 @@
 var currentUser = require('../db/currentUser.js');
 
+/**
+ * User will be redirected to here when successfully authenticated by GoodReads
+ * This extra step is needed so 'status' and 'userId' can be set to cookie. They are needed to retrieve user data in 'usersRoutes.js'
+ * GET: redirect user to '/browse' page, so user can start to browse potential matches
+ * @type {Object}
+ */
+
 module.exports = {
   get: function( req, res){
     res.cookie('status', JSON.stringify('true'));
